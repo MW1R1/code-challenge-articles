1fcf19c96fe0c62d226534f2a751dd13b5f2bdd4
MW1R1 <nickmwirigi58@gmail.com>
2023-05-28 14:40:44 +0300

    app logic added

--- run.rb
+++ run.rb
@@ -9,3 +9,21 @@ db = SQLite3::Database.new('database.db')
 
 # Enable the results to be returned as a hash
 db.results_as_hash = true
+# Create a new author
+author = Author.new('John Doe')
+
+# Create a new magazine
+magazine = Magazine.new('Magazine Name', 'Category')
+
+# Create a new article and associate it with the author and magazine
+article = Article.new(author, magazine, 'Article Title')
+
+# Print the article title
+puts article.title
+
+# Print the author's name
+puts author.name
+
+# Print the magazine's name
+puts magazine.name
+puts magazine.category
