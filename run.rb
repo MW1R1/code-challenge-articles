3f6ab85459d0e38fb191c51a29497e71fad20174
MW1R1 <nickmwirigi58@gmail.com>
2023-05-28 14:38:00 +0300

    establish db connection

--- run.rb
+++ run.rb
@@ -2,3 +2,10 @@ require_relative 'main'
 require_relative 'author'
 require_relative 'magazine'
 require_relative 'article'
+require 'sqlite3'
+
+# Create a new SQLite3 database object
+db = SQLite3::Database.new('database.db')
+
+# Enable the results to be returned as a hash
+db.results_as_hash = true
